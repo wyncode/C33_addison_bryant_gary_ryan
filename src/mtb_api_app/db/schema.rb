e748b34fec77c1152de4928c9535c5374da9828a
Addison Grillo <addisongrillo@gmail.com>
2019-05-29 22:46:46 -0400

    Converted to PostGres

--- src/mtb_api_app/db/schema.rb
+++ src/mtb_api_app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_25_190128) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"

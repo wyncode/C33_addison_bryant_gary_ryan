0878d0fbb775e41cb7629630f497e645969f2114
Addison Grillo <addisongrillo@gmail.com>
2019-06-12 10:43:09 -0400

    Pull from Heroku

--- src/mtb_api_app/app/models/trail.rb
+++ src/mtb_api_app/app/models/trail.rb
@@ -1,4 +1,27 @@
 class Trail < ApplicationRecord
     belongs_to :user
     has_many :sections, dependent: :destroy
+
+    def weather
+        two_four_ago = Time.now.to_i
+        response = HTTParty.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{lon},#{two_four_ago}")
+        JSON.parse(response.body, :quirks_mode => true)
+    end
+
+    def hours_of_rain_since_midnight(weather_response)
+        weather_response["hourly"]["data"].select do |forecast|
+            forecast["time"] > 0 && 
+            forecast["time"] < Time.now.to_i &&
+            forecast["precipProbability"] > 0
+        end.count
+    end
+
+    def rain_probabilities_since_midnight(weather_response)
+        weather_response["hourly"]["data"].select do |forecast|
+            forecast["time"] > 0 && 
+            forecast["time"] < Time.now.to_i
+        end.map do |forecast_since_midnight|
+            [Time.at(forecast_since_midnight["time"]).hour, forecast_since_midnight["precipProbability"]]
+        end.to_h        
+    end
 end
